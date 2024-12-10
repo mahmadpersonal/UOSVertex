90ce0ec51c768a122a0f6ccad65fb7354ddadfe2
M-Haseeb-Aslam <haseebaslam116@gmail.com>
2024-12-11 00:21:35 +0500

    Comments and file opening by Haseeb.Logic by Fahad and Ahmad

--- module_2_final.cpp
+++ module_2_final.cpp
@@ -6,15 +6,39 @@
 using namespace std;
 int main()
 {
-    ifstream file("friends_dataset.csv")
-    if (!file.is_open())
+    ifstream file("friends_dataset.csv");
+    if(!file.is_open())
     {
         cout<<"Unable to open the file !"<<endl;
         return 1; //This indicated error
     }
                 //key       //values
-    unordered_map<string, set<string>>friendships;
+    unordered_map<string, set<string>> friendships;
+    string line; //Reads the lanes of the file
+    getline(file, line); //skips the header file at first call
+    while (getline(file, line))
+    {
+        stringstream ss(line);  //converts the string into stream for easy reading
+        string person, friendName;     //variables to hold person name and friend name 
+        getline(ss,person,',');     //gets 1st name as person's name
+        while (getline(ss,friendName,','))
+        {
+            if(!friendName.empty()){
+                friendships[person].insert(friendName); //adds the name into that persons friend list
+            }
+        }
 
-    
-
+    }
+    file.close();
+    cout<<"Asymmetric relationships :"<<endl;
+    for (const auto&[person,friends]:friendships)
+    {
+        for (const string friendName:friends)
+        {
+            if(friendships[friendName].find(person)==friendships[friendName].end())
+            cout<<person<<" lists "<<friendName<<" as friend but not vice versa"<<endl;
+        }
+        
+    }
+    return 0;
 }
