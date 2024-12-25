d81ec0d8741d9e0add613aba2b1ca1aeebbbd02c
M-Haseeb-Aslam <haseebaslam116@gmail.com>
2024-12-25 20:18:58 +0500

    Done

--- module_6_final.cpp
+++ module_6_final.cpp
@@ -3,24 +3,27 @@
 #include <unordered_map>
 #include <unordered_set>
 using namespace std;
-
 int main() {
     ifstream file("friends_dataset.csv");
     if (!file) {
         cout << "Error opening the file!" << endl;
         return 1;
     }
+
     unordered_map<string, unordered_set<string>> studentFriends;
     string line;
+
     while (getline(file, line)) {
-        string student = "", friendName = "";
+        string student = "";
         int i = 0;
+
         while (i < line.size() && line[i] != ',') {
             student += line[i++];
         }
+
         i++; 
         while (i < line.size()) {
-            friendName.clear();
+            string friendName = "";
             while (i < line.size() && line[i] != ',') {
                 friendName += line[i++];
             }
@@ -31,28 +34,27 @@ int main() {
         }
     }
     file.close();
+
     int a;
     cout << "Enter the minimum threshold (alpha) for potential friendships: ";
     cin >> a;
-    string students[100];
-    int studentCount = 0;
-    for (const auto& entry : studentFriends) {
-        students[studentCount++] = entry.first;
-    }
-    cout << "Potential friendships:\n";
-    for (int i = 0; i < studentCount; i++) {
-        for (int j = i + 1; j < studentCount; j++) {
-            const string& student1 = students[i];
-            const string& student2 = students[j];
-            if (studentFriends[student1].count(student2)) {
+
+    for (const auto& entry1 : studentFriends) {
+        for (const auto& entry2 : studentFriends) {
+            const string& student1 = entry1.first;
+            const string& student2 = entry2.first;
+
+            if (student1 == student2 || studentFriends[student1].count(student2)) {
                 continue;
             }
+
             int common = 0;
             for (const string& f : studentFriends[student1]) {
                 if (studentFriends[student2].count(f)) {
                     common++;
                 }
             }
+
             if (common >= a) {
                 cout << student1 << " and " << student2 << " are potential friends with " << common << " common friends.\n";
             }
